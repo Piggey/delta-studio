8dd9599c79de6b0a978c544c7af25f6ef861f2a3
Piggey <pepcio3320@gmail.com>
2022-09-09 17:27:09 +0200

    kinda janky `GetModulePath()` for linux systems

--- engines/basic/src/os_utilities.cpp
+++ engines/basic/src/os_utilities.cpp
@@ -2,17 +2,34 @@
 
 #include "../include/path.h"
 
-#include <Windows.h>
+#if defined(_MSC_VER)
+    #include <Windows.h>
+#elif defined(__GNUC__)
+    #include <unistd.h>
+#endif
 
 dbasic::Path dbasic::GetModulePath() {
-    // Windows only implementation for now
     char path[256];
+
+#if defined(_MSC_VER)
     DWORD result = GetModuleFileName(NULL, path, 256);
 
     Path fullPath = Path(path);
     Path parentPath;
 
     fullPath.GetParentPath(&parentPath);
+
+#elif defined(__GNUC__)
+    char id[256];
+    sprintf(id, "/proc/%d/exe", getpid());
+    readlink(id, path, 255);
+    path[255] = '\0';
+
+    Path fullPath = Path(path);
+    Path parentPath;
+    fullPath.GetParentPath(&parentPath);
+
+#endif
 
     return parentPath;
 }
