1c000de8301a9ac51b55aff8245dad8a0a1af250
Piggey <pepcio3320@gmail.com>
2022-09-09 17:26:18 +0200

    cross-platform demo based on SDL2

--- demos/delta-basic-demo/src/main.cpp
+++ demos/delta-basic-demo/src/main.cpp
@@ -1,6 +1,7 @@
-#include <Windows.h>
-
 #include "../include/demo_list.h"
+
+#if defined(_MSC_VER)
+    #include <Windows.h>
 
 int WINAPI WinMain(
     _In_ HINSTANCE hInstance,
@@ -12,9 +13,8 @@ int WINAPI WinMain(
     (void)lpCmdLine;
     (void)hPrevInstance;
 
-    ysContextObject::DeviceAPI api = ysContextObject::DeviceAPI::DirectX11;
-    delta_demo::DemoApplication::Demo demo =
-        delta_demo::DemoApplication::Demo::DeltaBasicDemo;
+    auto api = ysContextObject::DeviceAPI::DirectX11;
+    auto demo = delta_demo::DemoApplication::Demo::DeltaBasicDemo;
 
     delta_demo::DemoApplication *app = delta_demo::CreateApplication(demo);
     app->Initialize((void *)&hInstance, api); 
@@ -23,3 +23,45 @@ int WINAPI WinMain(
 
     return 0;
 }
+#else
+    #include <SDL2/SDL.h>
+
+    int main (int argc, char *argv[]) {
+
+        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+            fprintf(stderr, "SDL Could not initialize.\n%s\n", SDL_GetError());
+            return -1;
+        }
+
+        auto api = ysContextObject::DeviceAPI::OpenGL4_0;
+        auto demo = delta_demo::DemoApplication::Demo::DeltaBasicDemo;
+
+        SDL_Window* window = SDL_CreateWindow(
+            "DeltaBasicDemo",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            480,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
+        );
+
+        if (window == NULL) {
+            fprintf(stderr, "Could not create window.\n%s\n", SDL_GetError());
+            return -2;
+        }
+
+        SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+        auto* app = delta_demo::CreateApplication(demo);
+        app->Initialize(window, api);
+        app->Run();
+        app->Destroy();
+
+        // cleanup
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+
+        return 0;
+    }
+    
+#endif
