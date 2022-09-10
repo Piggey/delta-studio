4031aae597572f984c4d0ba33bf5b16390778795
Piggey <pepcio3320@gmail.com>
2022-09-10 02:22:37 +0200

    this shouldnt be here

--- demos/delta-basic-demo/src/main.cpp
+++ demos/delta-basic-demo/src/main.cpp
@@ -27,39 +27,13 @@ int WINAPI WinMain(
     #include <SDL2/SDL.h>
 
     int main (int argc, char *argv[]) {
-
-        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-            fprintf(stderr, "SDL Could not initialize.\n%s\n", SDL_GetError());
-            return -1;
-        }
-
         auto api = ysContextObject::DeviceAPI::OpenGL4_0;
         auto demo = delta_demo::DemoApplication::Demo::DeltaBasicDemo;
-
-        SDL_Window* window = SDL_CreateWindow(
-            "DeltaBasicDemo",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            640,
-            480,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
-        );
-
-        if (window == NULL) {
-            fprintf(stderr, "Could not create window.\n%s\n", SDL_GetError());
-            return -2;
-        }
-
-        SDL_Surface* surface = SDL_GetWindowSurface(window);
-
         auto* app = delta_demo::CreateApplication(demo);
-        app->Initialize(window, api);
+
+        app->Initialize(api);
         app->Run();
         app->Destroy();
-
-        // cleanup
-        SDL_DestroyWindow(window);
-        SDL_Quit();
 
         return 0;
     }
